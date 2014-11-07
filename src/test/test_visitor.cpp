378042988a46803b83ef7e6e5f7183c6c439d70e
Delesley Hutchins <delesley@google.com>
2014-11-07 14:06:14 -0800

    Added test code for DefaultReducer.

--- src/test/test_visitor.cpp
+++ src/test/test_visitor.cpp
@@ -25,10 +25,53 @@
 namespace ohmu {
 namespace til  {
 
+
+/// This is an alternative implementation of visitor, which just uses
+/// DefaultReducer.  It is here as a test case for DefaultReducer.
+template<class Self>
+class AlternateVisitor : public Traversal<Self, VisitReducerMap>,
+                         public DefaultReducer<Self, VisitReducerMap>,
+                         public DefaultScopeHandler<VisitReducerMap> {
+public:
+  typedef Traversal<Self, VisitReducerMap> SuperTv;
+
+  Self* self() { return static_cast<Self*>(this); }
+
+  AlternateVisitor() : Success(true) { }
+
+  bool visitSExpr(SExpr& Orig) {
+    return true;
+  }
+
+  bool reduceSExpr(SExpr& Orig) {
+    return self()->visitSExpr(Orig);
+  }
+
+  /// Abort traversal on failure.
+  template <class T>
+  MAPTYPE(VisitReducerMap, T) traverse(T* E, TraversalKind K) {
+    Success = Success && SuperTv::traverse(E, K);
+    return Success;
+  }
+
+  static bool visit(SExpr *E) {
+    Self Visitor;
+    return Visitor.traverseAll(E);
+  }
+
+private:
+  bool Success;
+};
+
+
 class SimpleVisitor : public Visitor<SimpleVisitor> { };
+
+class SimpleVisitor2 : public AlternateVisitor<SimpleVisitor2> { };
+
 
 void test(SExpr* E) {
   SimpleVisitor::visit(E);
+  SimpleVisitor2::visit(E);
 }
 
 
