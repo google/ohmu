b1878b3c5b3115d8801593cb7ded26266671c151
Bart van Delft <bvd@google.com>
2015-08-25 10:14:37 -0700

    Replacing the way in which we swap standalone and pregel computations. Since the compilation of separate translation units for analyses (such as SCCComputation.cpp) will not pass through any macros set by the files calling these computations there is no way to control the framework selected. For now, just change the include in GraphComputation.h depending on context.

--- src/lsa/GraphComputation.h
+++ src/lsa/GraphComputation.h
@@ -1,18 +1,20 @@
+//===- GraphComputation.h --------------------------------------*- C++ --*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License.  See LICENSE.TXT in the LLVM repository for details.
+//
+//===----------------------------------------------------------------------===//
+// The purpose of this level of indirection is to easily replace the standalone
+// framework with a different framework providing the same GraphComputation and
+// GraphVertex interface.
+//===----------------------------------------------------------------------===//
+
 #ifndef OHMU_LSA_GRAPHCOMPUTATION_H
 #define OHMU_LSA_GRAPHCOMPUTATION_H
 
-/// To specify a pregel computation, include these defines before importing
-/// GraphComputation.h
-// #define GRAPH_COMPUTATION_MODE_PREGEL 1
-// #define NAME_AS_STRING(name) #name
-// #define PREGEL_GRAPH_COMPUTATION_PATH \
-//   NAME_AS_STRING(path/to/PregelGraphComputation.h)
-
-#if GRAPH_COMPUTATION_MODE_PREGEL
-#include PREGEL_GRAPH_COMPUTATION_PATH
-#else
 #include "StandaloneGraphComputation.h"
-#endif
 
 /// To provide serialization in Google's Pregel framework.
 template <class T> class StringCoderCustom;
